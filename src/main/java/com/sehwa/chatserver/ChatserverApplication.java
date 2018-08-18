a625596e4f14c5e62c3b4e64e1f3be23532b8dfd
SehwaKim <sehwakm@gmail.com>
2018-08-19 02:43:24 +0900

    Initiate new project

---
+++ src/main/java/com/sehwa/chatserver/ChatserverApplication.java
@@ -0,0 +1,12 @@
+package com.sehwa.chatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatserverApplication.class, args);
+    }
+}
